f98e1879ddbf76e12bf9216f70a7cc121c593da4
Michael Stromer <stroms38@gmail.com>
2020-01-27 11:29:52 -0500

    setup server

---
+++ src/entity/Place.ts
@@ -0,0 +1,25 @@
+import { Entity, BaseEntity, PrimaryGeneratedColumn, Column } from 'typeorm';
+import { ObjectType, Field, ID } from 'type-graphql';
+
+@ObjectType({ description: 'Destination or place of interest' })
+@Entity()
+export class Place extends BaseEntity {
+  @Field(() => ID)
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Field()
+  @Column()
+  title: string;
+
+  @Field({
+    nullable: true,
+    description: 'The place description with localized info'
+  })
+  @Column()
+  description?: string;
+
+  @Field()
+  @Column()
+  creationDate: Date;
+}
