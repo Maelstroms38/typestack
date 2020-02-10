cbe05a6355d0ba965178933de8b9b5717e42cc0e
Michael Stromer <stroms38@gmail.com>
2020-02-10 11:12:13 -0500

    include redis url

--- src/redis.ts
+++ src/redis.ts
@@ -2,10 +2,11 @@ import Redis from 'ioredis';
 import { RedisPubSub } from 'graphql-redis-subscriptions';
 
 // create Redis instance
-export const redis = new Redis();
+export const redis = new Redis(process.env.REDIS_URL);
 
 // configure Redis connection options
 const options: Redis.RedisOptions = {
+  path: process.env.REDIS_URL,
   keepAlive: 1000,
   retryStrategy: times => Math.max(times * 100, 3000)
 };
